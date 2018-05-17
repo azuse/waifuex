9bc07d5390f9ae1568be212d7cf30fda6cea14b6
徐凯 <1291843477@qq.com>
2018-05-17 18:49:00 +0900

    Unity版本启动

---
+++ Assets/Scripts/AutoChat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using System.Text;
+
+public class AutoChat : MonoBehaviour {
+
+	public InputField TextContent;
+	public GameObject DialogRect;
+	public Text TextView;
+	List<String> DialogList = new List<String> ();
+	String GetReply(String input){
+		return "Hello";
+	}
+
+	public void ChangeText(){
+		DialogList.Add (TextContent.text);
+		TextView.text += TextContent.text + "\n";
+		TextContent.text = GetReply (TextContent.text);
+		DialogList.Add (TextContent.text);
+		TextView.text += TextContent.text + "\n";
+	}
+
+	static bool print_flag = true;
+	public void PrintDialog(){
+		if (print_flag) {
+			DialogRect.SetActive (true);
+			String DialogContent = "";
+			for (int i = 0; i < DialogList.Count; i++) {
+				DialogContent += DialogList [i] + "\n";
+			}
+			TextView.text = DialogContent;
+		} else {
+			DialogRect.SetActive (false);
+			TextView.text = "";
+		}
+		print_flag = !print_flag;
+	}
+}
---
+++ Assets/Scripts/KoharuControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoharuControl : MonoBehaviour {
+
+	// Use this for initialization
+	protected GameObject Hibiki;
+	void Start () {
+		Hibiki = GameObject.Find ("hibiki");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Hibiki.transform.GetComponent<Animation> ().isPlaying) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				Hibiki.transform.GetComponent<Animation> ().Stop ();
+			} 
+		} else {
+			if (Input.GetKeyDown (KeyCode.Alpha1)) {
+				Hibiki.transform.GetComponent<Animation> ().Play ("hibiki_01");
+			}
+			if (Input.GetKeyDown (KeyCode.Alpha2)) {
+				Hibiki.transform.GetComponent<Animation> ().Play ("hibiki_02");
+			}
+			if (Input.GetKeyDown (KeyCode.Alpha3)) {
+				Hibiki.transform.GetComponent<Animation> ().Play ("hibiki_03");
+			}
+			if (Input.GetKeyDown (KeyCode.Alpha4)) {
+				Hibiki.transform.GetComponent<Animation> ().Play ("hibiki_04");
+			}
+			if (Input.GetKeyDown (KeyCode.Alpha5)) {
+				Hibiki.transform.GetComponent<Animation> ().Play ("hibiki_05");
+			}
+		}
+	}
+}
