27a0eafef732d15ca1bdd4e9e733a4033f798850
徐凯 <1291843477@qq.com>
2018-05-22 04:11:09 +0900

    SDK2.1更新
    只要模型本身支持，可以戳，可以更换模型，暂时只能更换内置的几个，等我研究下文件结构，看看怎么读绝对路径的模型，这次就这样。

--- Assets/Live2D/Cubism/Plugins/Readme.md
+++
@@ -1,4 +0,0 @@
-# Cubism 3.0 SDK Core Native Plugin
-
-This folder contains the Cubism SDK native plugin(s).
-*If you encounter exceptions upon first import, restart Unity*.
---
+++ Assets/MyScripts/UIController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+public class UIController : MonoBehaviour {
+
+	public GameObject SettingUI;
+	public Dropdown[] Dropdowns;//Language VoiceInput
+    public Text ModelPath;
+	Dictionary<String, String> Configs = new Dictionary<String, String>();
+
+	void Start(){
+		StreamReader SR = new StreamReader ("Config.ini", Encoding.Default);
+		String line;
+		while ((line = SR.ReadLine ()) != null) {
+			String[] cline = line.Split ('=');
+			Configs.Add (cline [0], cline [1]);
+		}
+		SR.Close ();
+		switch (Configs ["Language"]) {
+		case "Chinese":{
+				Dropdowns [0].value = 0;
+				break;
+			}
+		case "Japanese":{
+				Dropdowns [0].value = 1;
+				break;
+			}
+		}
+		switch (Configs ["VoiceInput"]) {
+		case "on":{
+				Dropdowns [1].value = 0;
+				break;
+			}
+		case "off":{
+				Dropdowns [1].value = 1;
+				break;
+			}
+		}
+        ModelPath.text = Configs["ModelPath"];
+	}
+
+	public void ShowSettings(){
+		SettingUI.SetActive (true);
+	}
+
+	public void HideSettings(){
+		SettingUI.SetActive (false);
+	}
+
+	public void ChangeSettings(){
+		switch (Dropdowns [0].value) {
+		case 0:{
+				Configs["Language"] = "Chinese";
+				break;
+			}
+		case 1:{
+				Configs["Language"] = "Japanese";
+				break;
+			}
+		}
+		switch (Dropdowns [1].value) {
+		case 0:{
+				Configs["VoiceInput"] = "on";
+				break;
+			}
+		case 1:{
+				Configs["VoiceInput"] = "off";
+				break;
+			}
+		}
+        Configs["ModelPath"] = ModelPath.text;
+    }
+
+	public void ConfirmSettings(){
+		StreamWriter SW = new StreamWriter("Config.ini", false);
+		for (int i = 0; i < Configs.Keys.Count; i++) {
+			String line = Configs.Keys.ElementAt(i) + "=" + Configs.Values.ElementAt(i);
+			SW.WriteLine (line);
+		}
+		SW.Close ();
+		HideSettings ();
+	}
+}
